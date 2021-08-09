0f07a703ff2619a53191c8b610f491d7865cb195
하찬균 <gkcksrbs@ajou.ac.kr>
2021-08-09 15:45:15 +0900

    2021-07-25

--- Week2/개인문제.py
+++ Week2/개인문제.py
@@ -1,12 +1,25 @@
+# 프로그래머스 고득점 Kit: 타겟 넘버
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+
+# 깊이 우선 탐색 알고리즘
 def dfs(lst, res, target):
+    # lst 에 원소가 없을 떄
     if not lst:
+        # target 값과 같은 결과가 나오면 1 반환, 나오지 않으면 0 반환
         if res == target:
             return 1
         else:
             return 0
+    # 재귀 함수
     return dfs(lst[1:], res+lst[0], target) + dfs(lst[1:], res-lst[0], target)
+
+
 def solution(numbers, target):
+    # 깊이 우선 탐색 알고리즘 실행
     answer = dfs(numbers, 0, target)
+    # 결과값 반환
     return answer
 
+
+# test code
 print(solution([1, 1, 1, 1, 1], 3))
--- Week2/공통문제.py
+++ Week2/공통문제.py
@@ -1,21 +1,34 @@
+# 2019 카카오 개발자 겨울 인턴십 : 튜플
+# https://programmers.co.kr/learn/courses/30/lessons/64065
+
 def solution(s):
     answer = []
-    test = tuple(map(str, s.split('},')))
     lst = []
+    # '},' 단위로 문자열 split
+    test = tuple(map(str, s.split('},')))
+
     for i in test:
+        # 문자열에서 '{', '}' 지우기
         tmp = i.replace('{', '')
         tmp = tmp.replace('}', '')
+        # 숫자 튜플들만 추가
         lst.append(set(map(int, tmp.split(','))))
+    # 길이 순서대로 정렬
     lst.sort()
 
     for i in lst:
+        # 길이가 1일 경우 첫 숫자만 추가
         if len(i) == 1:
             answer.append(list(i)[0])
+        # 길이가 2 이상의 튜플들에서 answer 리스트에 들어있지 않은 숫자들만 추가
         for j in i:
             if j not in answer:
                 answer.append(j)
+    # 결과값 반환
     return answer
 
+
+# test code
 # print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
 # print(solution("{{20,111},{111}}"))
 print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
