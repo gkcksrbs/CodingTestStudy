83c5d8898ab88c05fd2b1faae6e5aba9b4fff402
하찬균 <gkcksrbs@ajou.ac.kr>
2021-08-09 15:56:37 +0900

    2021-08-08

--- Week4/개인문제.py
+++ Week4/개인문제.py
@@ -1,12 +1,18 @@
+# 프로그래머스 고득점 Kit: 체육복
+# https://programmers.co.kr/learn/courses/30/lessons/42862
 def solution(n, lost, reserve):
+    # 여별을 갖고 온 학생 중 도난 당한 학생 제거, 도난 당한 학생 중 여벌을 갖고 온 학생 제거
     set_reserve = set(reserve) - set(lost)
     set_lost = set(lost) - set(reserve)
+    # 여벌을 갖고 온 학생들에서 양 옆 학생중 한명이 도난 당했다면 빌려주고 set_lost 배열에서 도난 당한 학생 제거
     for i in set_reserve:
         if i-1 in set_lost:
             set_lost.remove(i-1)
         elif i+1 in set_lost:
             set_lost.remove(i+1)
+    # 결과값 반환
     return n-len(set_lost)
 
 
+# test code
 print(solution(3, [3], [1]))
--- Week4/공통문제.py
+++ Week4/공통문제.py
@@ -1,12 +1,18 @@
+# 월간 코드 챌린지 시즌1 : 두 개 뽑아서 더하기
+# https://programmers.co.kr/learn/courses/30/lessons/68644
 from itertools import combinations
+
 
 def solution(numbers):
     answer = []
-
+    # 조합을 이용해서 2개의 숫자를 뽑고 더한 값을 answer 배열에 저장
     for i in combinations(numbers, 2):
         answer.append(sum(i))
-
+    # 중복값 제거
     answer = list(set(answer))
+    # 정렬 후 결과값 반환
     return sorted(answer)
 
+
+# test code
 print(solution([5,0,2,7]))
