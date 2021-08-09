3aa8c1de83f3c48b418912244e8cd04cdd1f4170
하찬균 <gkcksrbs@ajou.ac.kr>
2021-08-09 15:51:16 +0900

    2021-08-01

--- Week3/개인문제.py
+++ Week3/개인문제.py
@@ -1,11 +1,17 @@
+# 프로그래머스 고득점 Kit: 완주하지 못한 선수
+# https://programmers.co.kr/learn/courses/30/lessons/42576
+
 from collections import Counter
 
 
 def solution(participant, completion):
+    # 참가자 리스트, 완주 선수 리스트를 합친 후 Counter 로 생성
     answer = Counter(participant + completion)
+    # 해당 이름의 key 값이 홀수면 완주 못한 사람이므로 결과 반환
     for i in answer:
         if not answer[i] % 2 == 0:
             return i
 
 
+# test code
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
--- Week3/공통문제.py
+++ Week3/공통문제.py
@@ -1,11 +1,18 @@
+# 2021 카카오 채용연계형 인턴십 : 숫자 문자열과 영단어
+# https://programmers.co.kr/learn/courses/30/lessons/81301
+
+# key = 문자열, value: 숫자
 str_to_num = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
               'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
 
 
 def solution(s):
     for i in str_to_num:
+        # 영어를 숫자로 바꾸기
         s = s.replace(i, str_to_num[i])
+    # 정수형으로 결과값 반환
     return int(s)
 
 
+# test code
 print(solution("123"))
