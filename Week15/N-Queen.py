2b76f26cb53af4fe9e1c0b0c8511d55070025027
하찬균 <gkcksrbs@ajou.ac.kr>
2021-11-14 15:21:11 +0900

    2021-11-14

--- Week15/N-Queen.py
+++ Week15/N-Queen.py
@@ -1,43 +1,62 @@
 # 프로그래머스: 연습문제 N-Queen
 # https://programmers.co.kr/learn/courses/30/lessons/12952
+# def solution(n):
+#     board = [[0]*n for _ in range(n)]
+#     answer = n_queen(0, board)
+#     return answer
+#
+#
+# def n_queen(row, board):
+#     count = 0
+#     if row == len(board)-1:
+#         for i in range(len(board)):
+#             if not is_attack(row, i, board):
+#                 count += 1
+#     else:
+#         for i in range(len(board)):
+#             if not is_attack(row, i, board):
+#                 board[row][i] = 1
+#                 count += n_queen(row+1, board)
+#                 board[row][i] = 0
+#     return count
+#
+#
+# def is_attack(row, col, board):
+#     for i in range(row):
+#         if board[i][col] == 1:
+#             return True
+#     r, c = row-1, col-1
+#     while 0 <= r < len(board) and (0 <= c < len(board)):
+#         if board[r][c] == 1:
+#             return True
+#         r -= 1
+#         c -= 1
+#     r, c = row-1, col+1
+#     while 0 <= r < len(board) and 0 <= c < len(board):
+#         if board[r][c] == 1:
+#             return True
+#         r -= 1
+#         c += 1
+#     return False
 def solution(n):
-    board = [[0]*n for _ in range(n)]
-    answer = n_queen(0, board)
-    return answer
+    queen = [0]*n
+    return dfs(queen, n, 0)
 
 
-def n_queen(row, board):
+def dfs(queen, n, row):
     count = 0
-    if row == len(board)-1:
-        for i in range(len(board)):
-            if not is_attack(row, i, board):
-                count += 1
-    else:
-        for i in range(len(board)):
-            if not is_attack(row, i, board):
-                board[row][i] = 1
-                count += n_queen(row+1, board)
-                board[row][i] = 0
+    if n == row:
+        return 1
+    for col in range(n):
+        queen[row] = col
+        for x in range(row):
+            if queen[x] == queen[row]:
+                break
+            if abs(queen[x]-queen[row]) == (row-x):
+                break
+        else:
+            count += dfs(queen, n, row+1)
     return count
-
-
-def is_attack(row, col, board):
-    for i in range(row):
-        if board[i][col] == 1:
-            return True
-    r, c = row-1, col-1
-    while 0 <= r < len(board) and (0 <= c < len(board)):
-        if board[r][c] == 1:
-            return True
-        r -= 1
-        c -= 1
-    r, c = row-1, col+1
-    while 0 <= r < len(board) and 0 <= c < len(board):
-        if board[r][c] == 1:
-            return True
-        r -= 1
-        c += 1
-    return False
 
 
 if __name__ == '__main__':
