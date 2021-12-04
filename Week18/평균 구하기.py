4db508fd8877dc90bf05dbd291a8b032406c2754
하찬균 <gkcksrbs@ajou.ac.kr>
2021-12-04 22:14:01 +0900

    2021-12-04

---
+++ Week18/평균 구하기.py
@@ -0,0 +1,8 @@
+# 프로그래머스: 연습문제 평균 구하기
+# https://programmers.co.kr/learn/courses/30/lessons/12944
+def solution(arr):
+    return sum(arr) / len(arr)
+
+
+if __name__ == '__main__':
+    print(solution([1, 2, 3, 4]))
